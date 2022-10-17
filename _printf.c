200dd1e2780bd20961c9bc959f3a6e1e343df91b
Asasco <asamoahp08@gmail.com>
2022-10-17 07:17:44 -0700

    README

---
+++ _printf.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+
+void print_buffer(char buffer[], int *buff_ind);
+
+
+
+/**
+ *
+ *  * _printf - Printf function
+ *
+ *   * @format: format.
+ *
+ *    * Return: Printed c */
+
+int _printf(const char *format, ...)
+
+{
